f946ae86fd5b56eda1ecb254759f529735ccaf32
Vombit <45901675+Vombit@users.noreply.github.com>
2021-03-02 20:23:21 +0500

    Add files via upload

--- cmd/1_reload.js
+++ cmd/1_reload.js
@@ -1,29 +1,25 @@
-const Discord = module.require("discord.js");
-
-let rus = require('../assets/language/ru.json');
-let eng = require('../assets/language/en.json');
-
-module.exports.run = async (bot,message,args) => {
-    if(message.author.id != "*") return message.channel.send("Ты не мой хозяин")
-    if(!args[0]) return message.channel.send("выбери команду")
-    let cmdName = args[0].toLowerCase()
-    try {
-        delete require.cache[require.resolve(`./${cmdName}.js`)]
-        bot.commands.delete(cmdName)
-        const pull =require(`./${cmdName}.js`)
-        bot.commands.set(cmdName, pull)
-    } catch (e) {
-        return message.channel.send(`Не могу перезагрузить: \`${args[0].toUpperCase()}\``)
-    }
+module.exports.run = async (bot, message, args) => {
+	if(message.author.id != '317598066276565003') return;// message.channel.send('Ты не мой хозяин');
+	if(!args[0]) return message.channel.send('выбери команду');
+	const cmdName = args[0].toLowerCase();
+	try {
+		delete require.cache[require.resolve(`./${cmdName}.js`)];
+		bot.commands.delete(cmdName);
+		const pull = require(`./${cmdName}.js`);
+		bot.commands.set(cmdName, pull);
+	}
+	catch (e) {
+		return message.channel.send(`Не могу перезагрузить: \`${args[0].toUpperCase()}\``);
+	}
 
 };
 
 module.exports.help = {
-    name: "reload",
-    aliases: [""],
-    description: "",
-    usage: ".",
-    noalias: "No Aliases",
-    accessible: "",
-    enabled: true
+	name: 'reload',
+	aliases: ['rl'],
+	description: 'f',
+	usage: '.',
+	noalias: 'No Aliases',
+	accessible: '',
+	enabled: true
 };
